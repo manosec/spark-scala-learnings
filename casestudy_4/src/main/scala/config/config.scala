58cd94e2d5fe6e0d796705087c5ce5c299dc0119
Mano Ranjan N <86837128+manosec@users.noreply.github.com>
2025-01-02 18:37:55 +0530

    Delete casestudy_4 directory

--- casestudy_4/src/main/scala/config/config.scala
+++
@@ -1,5 +0,0 @@
-package config
-
-object config {
-  val serviceAccountPath = "/Users/mano/spark-scala-learnings/keys/spark-scala.json"
-}
--- casestudy_4/src/main/scala/kafkaStreamProcessing/salesDataSchema.scala
+++
@@ -1,3 +0,0 @@
-package kafkaStreamProcessing
-
-case class salesDataShema(store: String, dept: String, date: String, weeklySales: Float, is_Holiday: Boolean)
